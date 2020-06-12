625aa02e2d8e8bee6fb4597572cea73e308ffad5
Alex von Rhein <rhein@cqse.eu>
2020-06-12 11:28:19 +0200

    first try to catch segfaults

--- native/eu_cqse_clang_ClangBinding.cpp
+++ native/eu_cqse_clang_ClangBinding.cpp
@@ -2,7 +2,11 @@
 #include "Index.h"
 #include <cstdlib>
 #include <cstring>
+#include <cstdio>
 #include <mutex>
+#include <signal.h>
+#include <setjmp.h>
+
 
 #include "../../../clang/include/clang/Tooling/CompilationDatabase.h"
 #include "../../../clang-tools-extra/clang-tidy/ClangTidy.h"
@@ -299,10 +303,90 @@ JNIEXPORT jobject JNICALL Java_eu_cqse_clang_ClangBinding_getAllClangTidyCheckOp
     CLANG_JNI_END_EXCEPTION_HANDLER("getAllClangTidyCheckOptions")
 }
 
+jmp_buf ljenv;
+
+// this function will set the handler for a signal
+void setup_signal_handler(int sig, void (*handler)(  ), struct sigaction *old) {
+  struct sigaction action;
+
+  // fill action structure
+  // pointer to function that will handle signal
+  action.sa_handler = handler;
+  
+  // for the masks description take a look
+  // at "man sigaction"
+  sigemptyset(&(action.sa_mask));
+  sigaddset(&(action.sa_mask), sig);
+
+  // you can bring back original signal using
+  // SA_RESETHAND passed inside sa_flags
+  action.sa_flags = 0;
+
+  // and set new handler for signal 
+  sigaction(sig, &action, &actions[sig - 1]);
+}
+
+// this function will be called whenever signal occurs
+void handler(int handle) {
+  // be very condense here
+  // just do essential stuff and get
+  // back to the place you want to be
+  write(STDOUT_FILENO, "Hello from handler\n", strlen("Hello from handler\n"));
+  // set original signal handler
+  sigaction(handle, &actions[handle - 1], NULL);
+  // and jump to where we have set long jump
+  siglongjmp(ljenv, 1);
+}
+
+// riskCode protected by handler
+// this method will not crash JVM
 JNIEXPORT jobject JNICALL Java_eu_cqse_clang_ClangBinding_runClangTidyInternal
 (JNIEnv *env, jclass cls, jobject files, jstring rules, jobject compilerSwitches,
  jobject checkOptionsKeys, jobject checkOptionsValues, jboolean codeIsCpp) {
 
+	 
+	// OS X uses SIGBUS in case of accessing incorrect memory region
+	// Linux will use SIGSEGV - this is why we should use two handlers
+	// there are 31 possible signals we can handle
+	struct sigaction actions[31];
+	
+  // setup signal handlers 
+  // signals are counted from 1 - 31. Array indexes are
+  // counted from 0 - 30. This is why we do 10-1 and 11-1
+  setup_signal_handler(10, handler, &actions[10 - 1]);
+  setup_signal_handler(11, handler, &actions[11 - 1]);
+
+  jobject result;
+  // set the long jump for the signal handler
+  // if handler is called it will jump
+  // here with the error code specified
+  // as parameter of siglongjmp
+  // first call to sigsetjmp returns 0
+  if( sigsetjmp(ljenv,1) == 0) {
+    // call the code that will fail
+    result = runClangTidyInternal2(env, cls, files, rules, compilerSwitches, checkOptionKeys, checkOptionValues, codeIsCpp);
+  } else {
+    // we can allocate a little bit more than we require
+    char exceptionBuffer[1024];
+    sprintf(exceptionBuffer, "Error");
+    (*env)->ThrowNew(	env, 
+                      (*env)->FindClass( 	env, 
+                      "java/lang/Exception"), 
+                      exceptionBuffer);
+  }
+
+  // if everything was OK, we can set old handlers
+  sigaction(10, &actions[10 - 1], NULL);
+  sigaction(11, &actions[11 - 1], NULL);	
+  
+  return result;
+}
+
+
+jobject runClangTidyInternal2
+(JNIEnv *env, jclass cls, jobject files, jstring rules, jobject compilerSwitches,
+ jobject checkOptionsKeys, jobject checkOptionsValues, jboolean codeIsCpp) {
+	
     CLANG_JNI_BEGIN_EXCEPTION_HANDLER;
         
     jobject result = env->NewObject (jni_helper::arrayListClass, jni_helper::arrayListConstructor);
